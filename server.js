f325a32ba3cbe4cb6e89597792b6427d78675ba9
polumounika <polumounika0909@gmail.com>
2026-09-18 15:47:23 +0530

    Add backend dependencies and deployment configuration

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const session = require("express-session");
 
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: "http://localhost:4200",
   credentials: true
