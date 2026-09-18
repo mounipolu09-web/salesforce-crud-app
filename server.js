000b3f358361dced103e84e0824247868564dc3e
polumounika <polumounika0909@gmail.com>
2026-09-18 18:58:39 +0530

    Configure frontend CORS

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 app.use(cors({
-  origin: "http://localhost:4200",
+  origin: process.env.FRONTEND_URL || "http://localhost:4200",
   credentials: true
 }));
 app.use(express.json());
